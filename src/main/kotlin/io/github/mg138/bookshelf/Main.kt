dec23dc36dcb3f8922adeb9344e0b7dae6cd1a51
Mg138 <muguang138@gmail.com>
2021-12-28 20:34:58 +0800

    removed redundant stuff

--- src/main/kotlin/io/github/mg138/bookshelf/Main.kt
+++ src/main/kotlin/io/github/mg138/bookshelf/Main.kt
@@ -9,7 +9,6 @@ import io.github.mg138.bookshelf.damage.DamageManager
 import io.github.mg138.bookshelf.effect.Bleeding
 import io.github.mg138.bookshelf.effect.Burning
 import io.github.mg138.bookshelf.entity.test.DummyEntity
-import io.github.mg138.bookshelf.item.event.BookItemPrevention
 import io.github.mg138.bookshelf.item.test.FabricItem
 import io.github.mg138.bookshelf.stat.type.Preset
 import io.github.mg138.bookshelf.stat.type.StatTypeManager
@@ -41,7 +40,6 @@ object Main : DedicatedServerModInitializer {
             logger.info("Entity test enabled.")
         }
 
-        BookItemPrevention.register()
         DamageIndicatorManager.register()
         DamageManager.register()
         StatTypeCmd.register()
